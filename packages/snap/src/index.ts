aedf93ecee1dec5b4570f97ab3d8841c56a968ca
deependujha <deependujha21@gmail.com>
2022-11-18 19:55:13 +0530

    trying metamask snap

---
+++ packages/snap/src/index.ts
@@ -0,0 +1,41 @@
+import { OnRpcRequestHandler } from '@metamask/snap-types';
+
+/**
+ * Get a message from the origin. For demonstration purposes only.
+ *
+ * @param originString - The origin string.
+ * @returns A message based on the origin.
+ */
+export const getMessage = (originString: string): string =>
+  `Hello, world ${originString}!`;
+
+/**
+ * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
+ *
+ * @param args - The request handler args as object.
+ * @param args.origin - The origin of the request, e.g., the website that
+ * invoked the snap.
+ * @param args.request - A validated JSON-RPC request object.
+ * @returns `null` if the request succeeded.
+ * @throws If the request method is not valid for this snap.
+ * @throws If the `snap_confirm` call failed.
+ */
+export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
+  switch (request.method) {
+    case 'hello':
+      return wallet.request({
+        method: 'snap_confirm',
+        params: [
+          {
+            prompt: getMessage(origin),
+            description:
+              'Learning Metamask Snap. 😎',
+            textAreaContent:
+              'Learning how to tweak metamask snap text area. Huihuihui. 😈',
+          },
+        ],
+      });
+    default:
+      throw new Error('Method not found.');
+  }
+};
